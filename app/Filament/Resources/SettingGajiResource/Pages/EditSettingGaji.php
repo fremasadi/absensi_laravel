2bc80640fa487c08b5deb0ef73b930cec8709932
fremasadi <fremasadii@gmail.com>
2025-03-07 19:49:50 +0700

    uncommand

--- app/Filament/Resources/SettingGajiResource/Pages/EditSettingGaji.php
+++ app/Filament/Resources/SettingGajiResource/Pages/EditSettingGaji.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\SettingGajiResource\Pages;
 use App\Filament\Resources\SettingGajiResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\Facades\Artisan;
+
 
 class EditSettingGaji extends EditRecord
 {
