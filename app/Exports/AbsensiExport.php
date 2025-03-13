9adeb26f38f166c1ebb974171a294c45f13fc51d
fremasadi <fremasadii@gmail.com>
2025-03-13 15:25:35 +0700

    feat:order completed where userID

--- app/Exports/AbsensiExport.php
+++ app/Exports/AbsensiExport.php
@@ -3,18 +3,54 @@
 namespace App\Exports;
 
 use App\Models\Absensi;
-use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class AbsensiExport implements FromCollection, WithHeadings
+class AbsensiExport implements FromQuery, WithHeadings, WithMapping
 {
-    public function collection()
+    use Exportable;
+
+    public function query()
     {
-        return Absensi::select('user_id', 'tanggal_absen', 'waktu_masuk_time', 'waktu_keluar_time', 'durasi_hadir', 'status_kehadiran', 'keterangan')->get();
+        return Absensi::query()->with('user', 'jadwal')->select([
+            'id_user',
+            'id_jadwal',
+            'tanggal_absen',
+            'waktu_masuk_time',
+            'waktu_keluar_time',
+            'durasi_hadir',
+            'status_kehadiran',
+            'keterangan'
+        ]);
     }
 
     public function headings(): array
     {
-        return ['User ID', 'Tanggal Absen', 'Waktu Masuk', 'Waktu Keluar', 'Durasi Hadir', 'Status Kehadiran', 'Keterangan'];
+        return [
+            'Nama Karyawan', 
+            'Nama Jadwal',
+            'Tanggal Absen',
+            'Waktu Masuk',
+            'Waktu Keluar',
+            'Durasi Hadir (Menit)',
+            'Status Kehadiran',
+            'Keterangan',
+        ];
+    }
+
+    public function map($absensi): array
+    {
+        return [
+            $absensi->user->name ?? 'Tidak Diketahui',  // Relasi ke User
+            $absensi->jadwal->name ?? 'Tidak Ada Jadwal',  // Relasi ke Jadwal
+            $absensi->tanggal_absen,
+            $absensi->waktu_masuk_time,
+            $absensi->waktu_keluar_time,
+            $absensi->durasi_hadir,
+            $absensi->status_kehadiran,
+            $absensi->keterangan,
+        ];
     }
 }
