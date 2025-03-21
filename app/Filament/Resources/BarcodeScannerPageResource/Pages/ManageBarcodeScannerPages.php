c5991854516bdfaad64d254c06960815ac54d653
fremasadi <fremasadii@gmail.com>
2025-03-22 00:51:43 +0700

    feat:page scanner

--- app/Filament/Resources/BarcodeScannerPageResource/Pages/ManageBarcodeScannerPages.php
+++ app/Filament/Resources/BarcodeScannerPageResource/Pages/ManageBarcodeScannerPages.php
@@ -5,13 +5,12 @@ namespace App\Filament\Resources\BarcodeScannerPageResource\Pages;
 use App\Filament\Resources\BarcodeScannerPageResource;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\Page;
-use App\Filament\Components\BarcodeScanner;
 
 class ManageBarcodeScannerPages extends Page
 {
     protected static string $resource = BarcodeScannerPageResource::class;
 
-    protected static string $view = 'filament.components.scan-qr-code'; // Sesuaikan dengan path view Anda
+    protected static string $view = 'filament.pages.barcode-scanner'; // Sesuaikan dengan path view Anda
 
     protected function getHeaderActions(): array
     {
