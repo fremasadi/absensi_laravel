cd37ba43d90bdb87ee131ea15ba526b6ccfca6b5
fremasadi <fremasadii@gmail.com>
2025-03-22 01:57:03 +0700

    feat:page scanner

--- app/Filament/Resources/AbsensiResource.php
+++ app/Filament/Resources/AbsensiResource.php
@@ -12,6 +12,8 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Tables\Filters\Filter;
+use Filament\Tables\Filters\SelectFilter;
 
 class AbsensiResource extends Resource
 {
