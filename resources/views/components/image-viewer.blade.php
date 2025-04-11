fbb7685cd9296d2bbb6077c597c0b19c985781cb
fremasadi <fremasadii@gmail.com>
2025-04-11 23:09:25 +0700

    put:resize image absensi

--- resources/views/components/image-viewer.blade.php
+++ resources/views/components/image-viewer.blade.php
@@ -6,12 +6,3 @@
     >
 </div>
 
-<div class="flex justify-center mt-4">
-    <a 
-        href="{{ $imageUrl }}" 
-        target="_blank" 
-        class="px-4 py-2 bg-primary-500 text-white rounded hover:bg-primary-600 transition"
-    >
-        Buka Gambar di Tab Baru
-    </a>
-</div>
