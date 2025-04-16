fcffca23ecfe4a34eca9a5e593191fc6cc9e3566
fremasadi <fremasadii@gmail.com>
2025-04-16 22:55:58 +0700

    feat:print pdf gaji

--- app/Http/Controllers/GajiController.php
+++ app/Http/Controllers/GajiController.php
@@ -4,21 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Barryvdh\DomPDF\Facade\Pdf;
+use App\Models\Gaji; // ⬅️ Tambahkan ini
+use Carbon\Carbon;
 
 class GajiController extends Controller
 {
     public function downloadSlipGaji($id)
-{
-    $gaji = Gaji::with('user')->findOrFail($id);
+    {
+        $gaji = Gaji::with('user')->findOrFail($id);
 
-    $pdf = Pdf::loadView('pdf.slip-gaji-simple', [
-        'nama' => $gaji->user->name,
-        'periode_awal' => $gaji->periode_awal,
-        'periode_akhir' => $gaji->periode_akhir,
-        'perusahaan' => 'TOKOKITA',
-        'alamat' => 'Jl. Contoh Alamat No. 123, Kota Anda'
-    ]);
+        $pdf = Pdf::loadView('pdf.slip-gaji-simple', [
+            'nama' => $gaji->user->name,
+            'periode_awal' => $gaji->periode_awal,
+            'periode_akhir' => $gaji->periode_akhir,
+            'perusahaan' => 'TOKOKITA',
+            'alamat' => 'Jl. Contoh Alamat No. 123, Kota Anda'
+        ]);
 
-    return $pdf->download("Slip_Gaji_{$gaji->user->name}_Tokokita.pdf");
-}
+        return $pdf->download("Slip_Gaji_{$gaji->user->name}_Tokokita.pdf");
+    }
 }
