971df838ae397fd1845f2e3e8e6c8f58adbb3c66
fremasadi <fremasadii@gmail.com>
2025-04-17 13:31:54 +0700

    fix

--- app/Filament/Resources/JadwalShiftResource.php
+++ app/Filament/Resources/JadwalShiftResource.php
@@ -12,6 +12,7 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\Select;
 
 class JadwalShiftResource extends Resource
 {
