e31c88b8cd1c3ec4c27b3988d3287632714819a1
fremasadi <fremasadii@gmail.com>
2025-04-23 22:48:40 +0700

    feat:dashboard

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -52,8 +52,8 @@ class AdminPanelProvider extends PanelProvider
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
-                Dashboard::class,
-            ])
+                \Filament\Pages\Dashboard::class, // Use default Filament dashboard
+                ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 // Widgets\AccountWidget::class,
