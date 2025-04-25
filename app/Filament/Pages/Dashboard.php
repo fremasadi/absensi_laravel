844d0c2696dc7c9626dd9f066c5787e92f2a6abe
fremasadi <fremasadii@gmail.com>
2025-04-25 21:27:03 +0700

    feat

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -1,27 +1,23 @@
 <?php
 
+// app/Filament/Pages/Dashboard.php
+
 namespace App\Filament\Pages;
 
 use Filament\Pages\Dashboard as BaseDashboard;
 
 class Dashboard extends BaseDashboard
 {
-    protected static ?string $navigationIcon = 'heroicon-o-home';
-
     protected function getHeaderWidgets(): array
     {
         return [
-            // \App\Filament\Widgets\DashboardStats::class,
-            // \App\Filament\Widgets\AttendanceChartWidget::class,
-
+            \App\Filament\Widgets\AttendanceChartWidget::class,
         ];
     }
 
-    protected function getFooterWidgets(): array
+    protected function getFooter(): ?\Illuminate\View\View
     {
-        return [
-            \App\Filament\Widgets\AttendanceChartWidget::class,
-
-        ];
+        // Inject Chart.js ke footer HTML
+        return view('filament.custom-chart-js');
     }
 }
--- resources/views/filament/pages/dashboard.blade.php
+++ resources/views/filament/pages/dashboard.blade.php
@@ -1,3 +1,10 @@
-<x-filament-panels::page>
+{{-- resources/views/filament/pages/dashboard.blade.php --}}
+<x-filament::page>
+    <x-slot name="head">
+        {{-- Inject Chart.js di sini --}}
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    </x-slot>
 
-</x-filament-panels::page>
+    {{-- Kalau kamu render widget manual --}}
+    @livewire('filament.widgets.attendance-chart-widget')
+</x-filament::page>
