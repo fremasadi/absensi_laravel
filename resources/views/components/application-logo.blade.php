4231f81bcc750d4a1a20c87831597575ced6fa1f
fremasadi <fremasadii@gmail.com>
2025-04-27 22:09:38 +0700

    feat

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,4 +1,4 @@
-<!-- resources/views/components/application-logo.blade.php -->
+{{-- <!-- resources/views/components/application-logo.blade.php -->
 <span style="font-size: 24px; font-weight: bold; color: #000000;">
     TOKOKITA
-</span>
+</span> --}}
