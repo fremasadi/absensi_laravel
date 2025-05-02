9a74a1420e623c062233bb1764aff355b5dcfccb
fremasadi <fremasadii@gmail.com>
2025-05-02 13:59:08 +0700

    feat edit

--- app/Filament/Widgets/SalaryChartWidget.php
+++ app/Filament/Widgets/SalaryChartWidget.php
@@ -6,53 +6,53 @@ use App\Models\Gaji;
 use Carbon\Carbon;
 use Filament\Widgets\ChartWidget;
 
-// class SalaryChartWidget extends ChartWidget
-// {
-//     protected static ?string $heading = 'Salary Distribution';
-//     protected static ?int $sort = 3;
-//     protected int | string | array $columnSpan = 'full';
+class SalaryChartWidget extends ChartWidget
+{
+    protected static ?string $heading = 'Salary Distribution';
+    protected static ?int $sort = 3;
+    protected int | string | array $columnSpan = 'full';
     
-//     protected function getData(): array
-//     {
-//         // Get the last 6 months
-//         $months = collect(range(0, 5))->map(function ($month) {
-//             $date = Carbon::now()->subMonths($month);
-//             return [
-//                 'label' => $date->format('M Y'),
-//                 'month' => $date->month,
-//                 'year' => $date->year,
-//             ];
-//         })->reverse()->values();
+    protected function getData(): array
+    {
+        // Get the last 6 months
+        $months = collect(range(0, 5))->map(function ($month) {
+            $date = Carbon::now()->subMonths($month);
+            return [
+                'label' => $date->format('M Y'),
+                'month' => $date->month,
+                'year' => $date->year,
+            ];
+        })->reverse()->values();
         
-//         $salaryData = [];
-//         $labels = [];
+        $salaryData = [];
+        $labels = [];
         
-//         foreach ($months as $monthData) {
-//             $labels[] = $monthData['label'];
+        foreach ($months as $monthData) {
+            $labels[] = $monthData['label'];
             
-//             $total = Gaji::whereMonth('periode_akhir', $monthData['month'])
-//                 ->whereYear('periode_akhir', $monthData['year'])
-//                 ->sum('total_gaji');
+            $total = Gaji::whereMonth('periode_akhir', $monthData['month'])
+                ->whereYear('periode_akhir', $monthData['year'])
+                ->sum('total_gaji');
                 
-//             $salaryData[] = $total;
-//         }
+            $salaryData[] = $total;
+        }
         
-//         return [
-//             'datasets' => [
-//                 [
-//                     'label' => 'Total Salary',
-//                     'data' => $salaryData,
-//                     'backgroundColor' => '#6366F1',
-//                     'borderColor' => '#4F46E5',
-//                     'tension' => 0.2,
-//                 ],
-//             ],
-//             'labels' => $labels,
-//         ];
-//     }
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Total Salary',
+                    'data' => $salaryData,
+                    'backgroundColor' => '#6366F1',
+                    'borderColor' => '#4F46E5',
+                    'tension' => 0.2,
+                ],
+            ],
+            'labels' => $labels,
+        ];
+    }
     
-//     protected function getType(): string
-//     {
-//         return 'line';
-//     }
-// }
+    protected function getType(): string
+    {
+        return 'line';
+    }
+}
