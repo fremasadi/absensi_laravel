629c900461745456b6ccd9d02a9929e04c5bbc87
fremasadi <fremasadii@gmail.com>
2025-05-02 14:03:12 +0700

    feat edit

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -1,7 +1,5 @@
 <?php
 
-// app/Filament/Pages/Dashboard.php
-
 namespace App\Filament\Pages;
 
 use Filament\Pages\Dashboard as BaseDashboard;
@@ -11,19 +9,19 @@ class Dashboard extends BaseDashboard
     protected function getHeaderWidgets(): array
     {
         return [
-            // \App\Filament\Widgets\AttendanceChartWidget::class,
+            \App\Filament\Widgets\DashboardStats::class,         // 1. Statistik umum
+            \App\Filament\Widgets\AttendanceChartWidget::class,  // 2. Grafik absensi
+            \App\Filament\Widgets\SalaryChartWidget::class,      // 3. Grafik gaji
         ];
     }
+
     public static function getNavigationLabel(): string
-{
-    return 'Beranda'; // Ini label sidebar
-}
+    {
+        return 'Beranda';
+    }
 
-public static function getNavigationIcon(): string
-{
-    return 'heroicon-o-home';
-}
-
-
-
+    public static function getNavigationIcon(): string
+    {
+        return 'heroicon-o-home';
+    }
 }
