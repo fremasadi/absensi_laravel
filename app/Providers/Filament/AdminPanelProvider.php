e5e1bac6f34fad0466ade30432023aa7fd895b19
fremasadi <fremasadii@gmail.com>
2025-05-02 14:04:26 +0700

    feat edit

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -11,7 +11,6 @@ use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
-use Filament\Widgets;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -58,10 +57,7 @@ class AdminPanelProvider extends PanelProvider
                 Dashboard::class, // hanya satu dashboard
                 ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
-            ->widgets([
-                
-                // \App\Filament\Widgets\DashboardStats::class,      // Use App namespace
-                
+            ->widgets([                
             
             ])
             ->middleware([
