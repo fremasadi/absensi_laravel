93efce7f76174ff43433e867812c087d5f26eb08
fremasadi <fremasadii@gmail.com>
2025-05-02 14:10:17 +0700

    feat edit

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -54,7 +54,7 @@ class AdminPanelProvider extends PanelProvider
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
-                Dashboard::class, // Pastikan hanya ini yang didaftarkan
+                // Dashboard::class, // Pastikan hanya ini yang didaftarkan
                 ])
             // ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([                
