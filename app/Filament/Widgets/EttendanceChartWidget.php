59c40e8203e61ad88ac8ec4cf40f929e64ac154e
fremasadi <fremasadii@gmail.com>
2025-05-02 14:35:48 +0700

    feat

--- app/Filament/Widgets/EttendanceChartWidget.php
+++ app/Filament/Widgets/EttendanceChartWidget.php
@@ -10,7 +10,7 @@ use Filament\Widgets\ChartWidget;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 
-class AttendanceChartWidget extends ChartWidget
+class EttendanceChartWidget extends ChartWidget
 {
     protected static ?string $heading = 'Total Jam Kehadiran Karyawan per Bulan';
     
